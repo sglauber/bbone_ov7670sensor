19ed16d82d15006f6dadbbd2ac5a7b416caea300
Glauber Silva <glauber.silva14@gmail.com>
2018-08-19 07:22:17 -0300

    Alteração do exemplo PWM Código que exemplifica a utilização da interface PWM. Aqui, houve uma melhora nos comentários, onde  adicionei informações para deixar mais claro os testes e as configurações utilizadas, além de ter decidido utilizar português para explicação.

--- pwn_setup.py
+++ pwn_setup.py
@@ -1,49 +1,50 @@
-# Analog values are variables
-# Digital values are used to represent if something is on or off 1 or 0
+# Diferente de valores digitais (0 ou 1) os valores analógicos são variáveis.
+# Valores digitais são usados pra representar informações como: ligado / desligado, aberto ou fechado.
+# Utilizando valores analógico por exemplo, podemos trabalhar com uma "porcentagem" em determinada situação.
+# Por exemplo uma porta está aberta, (valor lógico 1), mas... o quanto ela está aberta?
+# Esta informação nos seria dada através de uma informação analógico.
 
-# Here we're going to do analog writes or at least simulate analog writes using pulse with modulations
-# We can simulate analog outputs turning our voltage on and off really quiclky generating a analog signal.
-# Fow PWM we can use the purple PINS on our beagle bone black. (See the diagram).
-# Here I'm using PINs P8_13 and GND P8_1
+# Aqui nós vamos simular escritas analógicas usando PWM (Pulse Width Modulation) ou Modução por Largura de Pulso
+# Podemos simular valores analógicos ligando e desligando a tensão do nosso sistema rapidamente para assim, gerar a informação analógica.
+# Neste exemplo utilizaremos os pinos roxos.
+# Aqui utilizaremos os pinos do HEADER P8 (lado direito), P8_13 .
+# Estas informações estão representados no diagrama.
+
+# Para termos acesso a interface programável com python utilizaremos a biblioteca da Adafruit importando as configurações PWM.
 
 import Adafruit_BBIO.PWM as PWM
 
-# Setting up the PIN to use and its start values
-# Setting up the Duty cycle, in percentage as 0%
-# Setting up the frequency as 1000Hz or 1Mhz
-# Having a duty cycle of 0, it means that its ON for 0% of the time, if we try to read values here it should output 0V
+# Vamos configurar a saída que iremos utilizar.
+# Aqui iremos setar o nosso ciclo de trabalho (duty cycle), este valor é dado em porcentagem e também a frequência, em Hz.
+# PWM.start("PIN_Reference" DutyCycle %, Frequency Hz).
+# Configurando nossa saída, iremos iniciar em 0 ou seja a saída está desligada e não deve gerar nenhuma tensão.
+
 PWM.start("P8_13",0, 1000)
 
-# We can use the method PWM.set_duty_cycle() to re-write our previous value.
-# Having a duty cycle of 100%, means that the signal is ON for 100% of the time, and now it must output 3.3V on our reading
+# Através do metódo set_duty_cycle() podemos reescrever o nosso ciclo de trabalho
+# Tendo um ciclo de trabalho de 100%, o que significa que agora a nossa saída está a "todo o vapor" e deve gerar uma tensão de 3.3V.
 PWM.set_duty_cycle("P8_13",100)
 
-# Getting an input from the user is also possible
-# vcc = input("What your desired voltage?:")
-# duty = V/(3.365*100)
-# if (duty > 100):
-#   dc = 100
-duty = input("Whats the duty cycle that you want?: ")
+# Vamos utilizaar um input(), pedir ao usuário um valor, para assim setar o nosso ciclo de trabalho.
+# Pediremos a ele a tensão desejada e como iremos modificar o ciclo de trabalho converteremos o valor da tensão para a porcentagem correspondente.
+# A nossa tensão máxima é aproximadamente 3.365V
+# Devemos tomar cuidado para não ultrapassar este valor e para isto utilizaremo uma condicional, já que o nosso ciclo de trabalho não deve ser maior que 100%.
+# Nosso código terá a seguinte estrutura:
 
-# And then assigning it to our PWM
+tensao = input("Qual a tensão desejada?: ")
+ciclo = tensao/3.365*100
+if (ciclo > 100):
+    ciclo = 100
 
-PWM.set_duty_cycle("P8_13",duty)
+# Então reescreveramos nossa saída com o valor informado pelo usuário
 
-# We can change the frequency using
-# Changing the frequency doesn't really affect the PWM since its changing based on the duty cycle
+PWM.set_duty_cycle("P8_13",ciclo)
+
+# Podemos modificar a frequência através do metódo set_frequency()
+# Porém alterar a frequência neste caso não irá afetar em nada pois estamos nos baseando em nosso ciclo de trabalho.
 # PWM.set_frequency("P8_13", 100)
 
-
-# Stopping the settings
+# Devemos então parar as configurações
 PWM.stop("P8_13")
-# Cleaning up the settings
+# E limpar nossas saídas
 PWM.cleanup()
-
-
-
-# We can create dimable LEDS applying PWM to a circuit
-# Using PINS GND > P9_1 and PWM pins P9_14 and P9_22
-# Python code to control the bright of our LEDS.
-
-PWM.start("P8_14",0, 1000)
-PWM.start("P8_22",0, 1000)
